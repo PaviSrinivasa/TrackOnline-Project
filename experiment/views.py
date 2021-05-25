a9fe91fc4d18d3af84bf9894d0e568cb1541932b
PaviSrinivasa <pavithra.srinivasa@mpi.nl>
2021-05-25 11:36:21 +0200

    return edit to home page

--- experiment/views.py
+++ experiment/views.py
@@ -27,7 +27,7 @@ def add(request):
         else:
             note = 'The new experiment was not created, please try again!'
         new_form = InfoForm()
-        expInfo = Info.objects.all();
+        expInfo = Info.objects.all()
         return render(request, 'home.html', {'note': note, 'expInfo': expInfo, 'created_exp_pk':created_exp_pk,})
         #return render(request, 'add.html', {'addform': new_form, 'note': note, 'created_exp_pk':created_exp_pk,})
     else:
@@ -43,6 +43,8 @@ def edit_exp(request, id):
         if filled_form.is_valid():
             filled_form.save()
             form = filled_form
+            expInfo = Info.objects.all()
             note = 'The experiment has been updated!'
-            return render(request, 'edit_exp.html', {'note':note,'editform':form,'exp':exp})
+            return render(request, 'home.html', {'note':note,'expInfo': expInfo,'editform':form,'exp':exp})
+            #return render(request, 'edit_exp.html', {'note':note,'editform':form,'exp':exp})
     return render(request, 'edit_exp.html', {'editform':form,'exp':exp})
